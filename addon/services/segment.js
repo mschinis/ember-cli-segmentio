60b288315cafc37845ca18156deb651e0faac887
Michael Schinis <mschinis@gmail.com>
2016-01-13 17:47:20 +0000

    fix(segment): Adds segment service with back compatibility support for ember-cli-segment

---
+++ addon/services/segment.js
@@ -0,0 +1,55 @@
+import Ember from 'ember';
+
+export default Ember.Service.extend({
+  init(){
+    // Get configuration and cache it in the service.
+    this.set('config', this.container.lookupFactory('config:environment'));
+    return this._super(...arguments);
+  },
+
+  hasAnalytics: function() {
+    return !!(this.raw() && typeof this.raw() === "object");
+  },
+
+  log: function() {
+    if(this.config && this.config.segment && this.config.segment.LOG_EVENT_TRACKING) {
+      Ember.Logger.info('[Segment.io] ', arguments);
+    }
+  },
+
+  raw(){
+    return window.analytics;
+  },
+  trackPageView: function() {
+    if(this.hasAnalytics()) {
+      this.raw().page.apply(this, arguments);
+
+      this.log('trackPageView', arguments);
+    }
+  },
+  trackEvent: function(event, properties, options, callback) {
+    if(this.hasAnalytics()) {
+      this.raw().track(event, properties, options, callback);
+      this.log(event, properties, options);
+    }
+  },
+  identifyUser: function(userId, traits, options, callback) {
+    if(this.hasAnalytics()) {
+      this.raw().identify(userId, traits, options, callback);
+      this.log('identifyUser', traits, options);
+    }
+  },
+
+  group: function(groupId, traits, options, callback) {
+    if(this.hasAnalytics()) {
+      this.raw().group(groupId, traits, options, callback);
+      this.log('group', traits, options);
+    }
+  },
+  aliasUser: function(userId, previousId, options, callback) {
+    if(this.hasAnalytics()) {
+      this.raw().alias(userId, previousId, options, callback);
+      this.log('aliasUser', previousId, options);
+    }
+  }
+});
