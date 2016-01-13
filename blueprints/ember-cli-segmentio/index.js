3019cc130f42b01b05c07eee9162472a7fdf3537
Michael Schinis <mschinis@gmail.com>
2016-01-13 18:26:13 +0000

    fix(index): Fixes default generator

--- blueprints/ember-cli-segmentio/index.js
+++ blueprints/ember-cli-segmentio/index.js
@@ -1,6 +1,11 @@
 /*jshint node:true*/
 module.exports = {
   description: 'Default ember-cli-segmentio blueprint. Installs dependencies',
+
+  normalizeEntityName: function(entityName) {
+    // Normalize and validate entity name here.
+    return entityName;
+  },
   afterInstall: function(){
     return this.addBowerPackageToProject('analytics');
   }
