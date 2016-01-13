ea875d00105dc387e0839446773c11c86d9c2022
Michael Schinis <mschinis@gmail.com>
2016-01-13 10:53:49 +0000

    feat(index): Includes analytics.js during build time

--- index.js
+++ index.js
@@ -5,5 +5,10 @@ module.exports = {
   name: 'ember-cli-segmentio',
   afterInstall(){
     return this.addBowerPackageToProject('analytics');
+  },
+  included(app){
+    this._super.included(app);
+
+    app.import(app.bowerDirectory + 'analytics/analytics.min.js');
   }
 };
